b44f8cc7decc637e9c2b7ca8e044e7195e368bb4
Halerium <hal@erium.ai>
2023-01-18 16:49:00 +0000

    updated templates repo

--- workflow_templates/strategy_workspace/hooks/post_gen_project.py
+++ workflow_templates/strategy_workspace/hooks/post_gen_project.py
@@ -8,10 +8,9 @@ hu.file.assign_new_card_ids_to_tree('./')
 
 
 # To move directory one up
-#for f in os.listdir('./'):
-#    shutil.move(f, '../')
+for f in os.listdir('./'):
+    shutil.move(f, '../')
 
-#path = Path('./').resolve()
-#shutil.rmtree(path)
-
+path = Path('./').resolve()
+shutil.rmtree(path)
 
