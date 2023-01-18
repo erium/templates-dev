36149b7a701d48a200c745a6e8488426123e92dd
MakGre <Maksim.Greiner@gmail.com>
2023-01-18 17:45:44 +0100

    Update post_gen_project.py

--- workflow_templates/strategy_workspace/hooks/post_gen_project.py
+++ workflow_templates/strategy_workspace/hooks/post_gen_project.py
@@ -3,28 +3,15 @@ from pathlib import Path
 import shutil
 import halerium_utilities as hu
 
-try:
-    # assign new card ids
-    hu.file.assign_new_card_ids_to_tree('./')
+# assign new card ids
+hu.file.assign_new_card_ids_to_tree('./')
 
 
-    # To move directory one up
-    #for f in os.listdir('./'):
-    #    shutil.move(f, '../')
+# To move directory one up
+#for f in os.listdir('./'):
+#    shutil.move(f, '../')
 
-    path = Path('./').resolve()
-    #shutil.rmtree(path)
-    
-    raise Exception(str(path))
+path = Path('./').resolve()
+#shutil.rmtree(path)
 
-except Exception as exc:
-#    try:
-#        with open("../fail", "w") as f:
-#            f.write(repr(exc))
-#    except:
-#        pass
-    try:
-        with open("fail", "w") as f:
-            f.write(repr(exc))
-    except:
-        pass
+
