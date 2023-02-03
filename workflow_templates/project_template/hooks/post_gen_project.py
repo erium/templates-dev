99ba258d62a13b95d31234d21b8ea7e91612b747
Halerium <hal@erium.ai>
2023-02-03 09:39:13 +0000

    updated templates repo

--- workflow_templates/project_template/hooks/post_gen_project.py
+++ workflow_templates/project_template/hooks/post_gen_project.py
@@ -3,13 +3,18 @@ from pathlib import Path
 import shutil
 import halerium_utilities as hu
 
+print("assigning new ids")
 # assign new card ids
 hu.file.assign_new_card_ids_to_tree('./')
 
 
+print("Move one directory up")
 # To move directory one up
 for f in os.listdir('./'):
     shutil.move(f, '../')
 
+print("Delete obsolete dir")
 path = Path('./').resolve()
 shutil.rmtree(path)
+
+print("Done")
