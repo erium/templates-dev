c53802f006f20c7d52bb1fcd128b960fa8f5249e
Halerium <hal@erium.ai>
2024-08-07 16:12:09 +0000

    updated templates repo

--- templates/applications/meeting_analyzer/hooks/post_gen_project.py
+++ templates/applications/meeting_analyzer/hooks/post_gen_project.py
@@ -1,8 +1,6 @@
 import halerium_utilities as hu
 import uuid
 
-from halerium_utilities.board import Board
-
 # assign new card ids
 hu.file.assign_new_card_ids_to_tree('./')
 
@@ -12,12 +10,20 @@ try:
 except:
     target_id = str(uuid.uuid4())
 
+board_path = "chatbot.board"
 # set chatbot card id to target id
-board = Board.from_json("chatbot.board")
+board = hu.board.Board.from_json(board_path)
 for card in board.cards:
     if card.type == "bot":
+        old_id = card.id
         card.id = target_id
         break  # only 1 card
-board.to_json("chatbot.board")
+# adapt connections
+for edge in board.connections:
+    if edge.connections.source.id == old_id:
+        edge.connections.source.id = target_id
+    if edge.connections.target.id == old_id:
+        edge.connections.target.id = target_id
+board.to_json(board_path)
 
 print("Hal_Magic_Template_done.")
