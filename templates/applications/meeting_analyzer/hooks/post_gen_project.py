3e04ddde712fc49f30b1cdca882048341bf1d041
Halerium <hal@erium.ai>
2024-08-07 16:02:07 +0000

    updated templates repo

--- templates/applications/meeting_analyzer/hooks/post_gen_project.py
+++ templates/applications/meeting_analyzer/hooks/post_gen_project.py
@@ -1,15 +1,23 @@
-import os
-from pathlib import Path
-import shutil
 import halerium_utilities as hu
+import uuid
+
+from halerium_utilities.board import Board
 
 # assign new card ids
 hu.file.assign_new_card_ids_to_tree('./')
 
-## To move directory one up
-#for f in os.listdir('./'):
-#    shutil.move(f, '../')
-#path = Path('./').resolve()
-#shutil.rmtree(path)
+target_id = "{{cookiecutter.chatbot_card_id}}"
+try:
+    target_id = str(uuid.UUID(target_id, version=4))
+except:
+    target_id = str(uuid.uuid4())
+
+# set chatbot card id to target id
+board = Board.from_json("chatbot.board")
+for card in board.cards:
+    if card.type == "bot":
+        card.id = target_id
+        break  # only 1 card
+board.to_json("chatbot.board")
 
 print("Hal_Magic_Template_done.")
